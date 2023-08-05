fc118cca628f5150ecb5f6f44a971f20854d45d2
joeyfav <joefav161@gmail.com>
2023-08-05 13:39:30 +0100

    add project files and setup

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // create varaibles to accept first and second user input
-let a = parseInt(prompt("Enter first number"));
-let b = parseInt(prompt("Enter secon number"));
+let a = parseInt(prompt("Enter the current year"));
+let b = parseInt(prompt("Enter the year you were born"));
 
 // create an addition function
-function addition () {
+function subtraction () {
 //The addition function will return the addition for input a and b
-return a + b;
+return a - b;
 }
 // Invoke addition functgition to run the calculator
-console.log(addition());
+console.log(subtraction());
