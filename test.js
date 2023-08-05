b2efebb9075fa67d5ad792460c7fcbbd80ebf474
joeyfav <joefav161@gmail.com>
2023-08-05 10:47:19 +0100

    add project files and setup

---
+++ test.js
@@ -0,0 +1,8 @@
+let input = 2;
+let input2 = 4;
+let input3 = 5;
+let input4 = "glad"
+let input5 = "happy"
+let array = [a, b, "x", [3, 5], "world"];
+let division = input / input2;
+console.log(array[0]);
